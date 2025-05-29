6697e99e3b43d2de857ef1519a25ddbc68c3ad2f
Gerardo Uriburu <guribururomero@gmail.com>
2025-05-29 16:49:33 -0300

    Second commit

--- script.js
+++ script.js
@@ -2,10 +2,15 @@
 
 let respuestas = []
 
-function preguntas() { (e) =>
-    
-    a = prompt("Hola mundo");
-    console.log('a');
+function preguntas() {
+    nac = prompt("Ingrese su nacionalidad: ");
+    respuestas.push(nac);
+
+    col_fav = prompt("Ingrese su color favorito: ");
+    respuestas.push(col_fav);
+
+    nom_masc = prompt("Ingrese el nombre de su mascota: ");
+    respuestas.push(nom_masc);
 }
 
 
